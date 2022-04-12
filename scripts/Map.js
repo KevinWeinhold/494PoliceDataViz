f8f14a70a25f0a1087fd2f4f7fa65533bfd9451a
xAndreiLi <andreili@asu.edu>
2022-04-11 20:20:47 -0700

    Working heatmap and wrangled state data

--- scripts/Map.js
+++ scripts/Map.js
@@ -1,5 +1,6 @@
 var mapWidth, mapHeight, mapInnerWidth, mapInnerHeight;
 var mapMargin = { top: 10, bottom: 10, left: 10, right: 20 };
+var mapData, policeData;
 
 document.addEventListener("DOMContentLoaded", () => {
   mapSvg = d3.select("#Map");
@@ -16,6 +17,16 @@ document.addEventListener("DOMContentLoaded", () => {
   ]).then(function (values) {
     mapData = values[0];
     policeData = values[1];
+    console.log(policeData)
+    // stateData contains the number of occurences in each state
+    stateData = {};
+    policeData.forEach(e => {
+      if(stateData[e["Incident.Location.State"]] != null)
+        stateData[e["Incident.Location.State"]] += 1;
+      else 
+        stateData[e["Incident.Location.State"]] = 1;
+    });
+    console.log(stateData)
     drawMap();
   });
 });
@@ -28,10 +39,16 @@ function drawMap() {
     .fitSize([mapInnerWidth, mapInnerHeight], mapData);
   let geoPath = d3.geoPath().projection(usaProjection);
 
-  let selectedColorValue = d3.interpolateInferno;
-  let colorScale = d3.scaleLinear().domain([0]);
+  let min, max;
+  min = stateData[Object.keys(stateData).reduce((key, v) => stateData[v] < stateData[key] ? v : key)];
+  max = stateData[Object.keys(stateData).reduce((key, v) => stateData[v] > stateData[key] ? v : key)];
+  console.log(max)
+
+  let colorScale = d3.scaleSequential(d3.interpolateInferno)
+                      .domain([min,max]);
 
   let g = mapSvg.append("g");
+  drawColorScale(g, colorScale);
 
   g.selectAll(".stateMap")
     .data(mapData.features)
@@ -41,9 +58,48 @@ function drawMap() {
     .classed("stateMap", true)
     .attr("id", (d) => d.properties.postal)
     .attr("fill", (d) => {
-      return "black";
+      if(stateData[d.properties.postal] == null)
+        console.log(d.properties.postal)
+      return colorScale(stateData[d.properties.postal])
     })
     .on("click", function (d, i) {
-      drawLineChart(d.properties.name);
+      
     });
+}
+
+function drawColorScale(g, colorScale) {
+  const linearGradient = g.append("defs")
+                          .append("linearGradient")
+                          .attr("id", "linear-gradient");
+  linearGradient.selectAll("stop")
+                .data(colorScale.ticks()
+                .map((t, i, n) => ({ 
+                  offset: `${100*i/n.length}%`, 
+                  color: colorScale(t) })))
+                .enter()
+                  .append("stop")
+                  .attr("offset", d => d.offset)
+                  .attr("stop-color", d => d.color);
+  g.append("rect")
+   .attr('transform', `translate(20,100)`)
+   .attr("width", 200)
+   .attr("height", 20)
+   .style("fill", "url(#linear-gradient)");
+  const colorAxis = d3.axisBottom(d3.scaleLinear()
+                      .domain(colorScale.domain())
+                      .range([0,200]))
+                      .ticks(5).tickSize(-20);
+  g.append('g').call(colorAxis)
+   .attr('class','colorLegend')
+   .attr('transform','translate(20,120)')
+   .selectAll('text')
+   .style('text-anchor','end')
+   .attr('dx','-0px')
+   .attr('dy', '0px')
+   .attr('transform','rotate(-45)');
+   g.append('text')
+     .attr('x',20)
+     .attr('y',90)
+     .style('font-size','.9em')
+     .text("Shootings");
 }
